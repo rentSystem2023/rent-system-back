ce0766694b5600a4e58308bfe7f89138bbf4eff1
hhhunnn <gusdk2090@naver.com>
2024-05-17 11:48:53 +0900

    jang

--- back/src/main/java/com/rentcar/back/service/UserService.java
+++ back/src/main/java/com/rentcar/back/service/UserService.java
@@ -4,9 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.rentcar.back.dto.response.user.GetSignInUserResponseDto;
+import com.rentcar.back.dto.response.ResponseDto;
+import com.rentcar.back.dto.response.user.GetMyInfoResponseDto;
 
 @Service
 public interface UserService {
+
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser (String userId); 
+
+    ResponseEntity<? super GetMyInfoResponseDto> getMyInfo (String userId);
+
+    ResponseEntity<ResponseDto> myInfoModify(String userId);
 }
 
