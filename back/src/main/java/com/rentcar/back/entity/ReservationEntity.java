4d84c59c1c0ea891731eb8f58e61190f02ffda41
devKMC <allcasa@naver.com>
2024-05-20 12:01:58 +0900

    2024-05-20 12:00

--- back/src/main/java/com/rentcar/back/entity/ReservationEntity.java
+++ back/src/main/java/com/rentcar/back/entity/ReservationEntity.java
@@ -1,16 +1,36 @@
 package com.rentcar.back.entity;
 
+import com.rentcar.back.dto.request.reservation.PatchReservationRequestDto;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="reservation")
+@Table(name="reservation") // 매핑되는 테이블의 이름은 user , 클래스명하고 테이블명이 다르기 때문에 지정
+@Getter // DB에서 받아오기 위한 작업
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class ReservationEntity {
     
     @Id
     private String reservationCode;
-    private String userId;
-    private String insuranceType;
+    @NotBlank
+    private String nickName;
+    @NotBlank
+    private String carImageUrl;
+    @NotBlank
     private String reservationDate;
-    private String reservationState;
-    private String reservationPeriod;
-    private Integer companyCarCode;
-    
+    @NotBlank
+    private String rentCompany;
+
+
 }
---
+++ back/src/main/java/com/rentcar/back/service/implementation/ReservationServiceImplementation.java
@@ -0,0 +1,42 @@
+package com.rentcar.back.service.implementation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.rentcar.back.dto.response.ResponseDto;
+import com.rentcar.back.dto.response.reservation.GetReservationMyListResponseDto;
+import com.rentcar.back.entity.ReservationEntity;
+import com.rentcar.back.repository.ReservationRepository;
+import com.rentcar.back.repository.UserRepository;
+import com.rentcar.back.service.ReservationService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationServiceImplementation implements ReservationService {
+
+    private final UserRepository userRepository;
+    private final ReservationRepository reservationRepository;
+
+    @Override
+    public ResponseEntity<? super GetReservationMyListResponseDto> getReservationList(String userId) {
+
+        ReservationEntity reservationEntity;
+        try {
+            boolean isExistUser = userRepository.existsById(userId);
+            if (!isExistUser)
+                return ResponseDto.authenticationFailed();
+            // 예약 내역 확인
+            reservationEntity = reservationRepository.findByUserId(userId);
+
+            // 성공 작성
+            return GetReservationMyListResponseDto.success(reservationEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+    }
+}
