0408930903ce336d573bb04efe666dc708a30713
devKMC <allcasa@naver.com>
2024-05-21 16:43:57 +0900

    reservation

--- back/src/main/java/com/rentcar/back/repository/ReservationRepository.java
+++ back/src/main/java/com/rentcar/back/repository/ReservationRepository.java
@@ -1,27 +1,39 @@
 package com.rentcar.back.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.rentcar.back.entity.CompanyEntity;
-
 import com.rentcar.back.entity.ReservationEntity;
-import com.rentcar.back.entity.UserEntity;
+import com.rentcar.back.repository.resultSet.GetUserReservationResultSet;
 
-import jakarta.persistence.Tuple;
 @Repository
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, String> {
     ReservationEntity findByUserIdOrderByRegervationDateDesc(String userId);
 
+    @Query(value=
+        "SELECT " +
+        "CA.car_image_url as carImageUrl, " +
+        "U.nick_name as nickName, " +
+        "R.reservation_date as reservationDate, " +
+        "R.reservation_code as reservationCode, " +
+        "CO.rent_company as rentCompany " +
+        "FROM reservation R " +
+        "INNER JOIN user U " +
+        "ON R.user_id = U.user_id " +
+        "INNER JOIN company_car CC " +
+        "ON R.company_car_code = CC.company_car_code " +
+        "INNER JOIN car CA " +
+        "ON CC.car_code = CA.car_code " +
+        "INNER JOIN company CO " +
+        "ON CC.company_code = CO.company_code " +
+        "WHERE R.user_id = :userId "
+    , nativeQuery=true)
+    public List<GetUserReservationResultSet> getUserReservationList(@Param("userId") String userId);
 
-    // @Query("SELECT r.user, r.company FROM ReservationEntity r WHERE r.userId = :userId")
-    // Tuple findUserAndCompanyByUserId(@Param("userId") String userId);
-
-    // @Query("SELECT c FROM CompanyEntity c WHERE c.companyCode = :companyCode")
-    // CompanyEntity findByCompanyCode(@Param("companyCode") Integer companyCode);
-    }
+}
 
     
