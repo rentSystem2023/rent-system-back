5d2cea1886f4b07e431a80e3b49b7042e5deb874
Hanseongyun <han832793@gmail.com>
2024-05-22 17:54:04 +0900

    05.22.reservation

--- back/src/main/java/com/rentcar/back/dto/request/reservation/PostReservationRequestDto.java
+++ back/src/main/java/com/rentcar/back/dto/request/reservation/PostReservationRequestDto.java
@@ -20,8 +20,7 @@ public class PostReservationRequestDto  {
     private String reservationEnd;
     @NotNull
     private Integer companyCarCode;
-    @NotBlank
-    private String reservationState;
+
 }
 
 
--- back/src/main/java/com/rentcar/back/repository/ReservationRepository.java
+++ back/src/main/java/com/rentcar/back/repository/ReservationRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rentcar.back.entity.ReservationEntity;
-import com.rentcar.back.repository.resultSet.GetCarCodeReservationResultSet;
+import com.rentcar.back.repository.resultSet.GetUserDetatilReservationResultSet;
 import com.rentcar.back.repository.resultSet.GetUserReservationResultSet;
 
 @Repository
@@ -34,4 +34,26 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
         "WHERE R.user_id = :userId"
     , nativeQuery=true)
     List<GetUserReservationResultSet> getUserReservationList(@Param("userId") String userId);
+
+    @Query(value = 
+        "SELECT " +
+        "CA.car_image_url as carImageUrl, " +
+        "U.nick_name as nickName, " +
+        "R.insurance_type as insuranceType, " +
+        "R.reservation_start as reservationStart, " +
+        "R.reservation_end as reservationEnd, " +
+        "CA.car_oil as carOil, " +
+        "CA.grade, " +
+        "CC.car_number as carNumber, " +
+        "CO.rent_company as rentCompany, " +
+        "CO.company_telnumber as companyTelnumber, " +
+        "CO.address " +
+        "FROM reservation R " +
+        "INNER JOIN user U ON R.user_id = U.user_id " +
+        "INNER JOIN company_car CC ON R.company_car_code = CC.company_car_code " +
+        "INNER JOIN car CA ON CC.car_code = CA.car_code " +
+        "INNER JOIN company CO ON CC.company_code = CO.company_code " +
+        "WHERE R.reservation_code = :reservationCode"
+    , nativeQuery = true)
+    List<GetUserDetatilReservationResultSet> getUserDetailReservationList(@Param("reservationCode") int reservationCode);
 }
