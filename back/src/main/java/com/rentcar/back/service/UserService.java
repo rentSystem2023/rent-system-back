c468e17e1c16471168dc101e0f961f769ec59934
hhhunnn <https://github.com/hhhunnn>
2024-05-31 09:28:21 +0900

    back

--- back/src/main/java/com/rentcar/back/service/UserService.java
+++ back/src/main/java/com/rentcar/back/service/UserService.java
@@ -31,8 +31,6 @@ public interface UserService {
 
     ResponseEntity<? super GetUserDetailListResponseDto> getUserDetailList (String userId);
 
-    ResponseEntity<ResponseDto> FindId(String userEmail);
-
     ResponseEntity<? super GetFindIdResponseDto> getFindId(String userEmail);
 
 }
