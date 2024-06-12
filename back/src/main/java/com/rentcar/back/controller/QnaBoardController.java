bb3cd46440d708ef23588a4e6d5a8cdafbea423c
hhhunnn <gusdk2090@naver.com>
2024-06-12 16:28:22 +0900

    sns로그인

--- back/src/main/java/com/rentcar/back/controller/QnaBoardController.java
+++ back/src/main/java/com/rentcar/back/controller/QnaBoardController.java
@@ -124,7 +124,6 @@ public class QnaBoardController {
     ) {
         ResponseEntity<? super GetSearchQnaBoardMyListResponseDto> response = qnaBoardService.getSearchQnaBoardMyList(word);
         return response;
-    
 
     }
 }
