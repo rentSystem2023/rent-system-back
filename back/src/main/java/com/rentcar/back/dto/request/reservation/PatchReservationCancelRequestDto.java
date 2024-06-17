c9e6fd567fa28690c2a8ebe0649a27e6a9e43d16
hhhunnn <https://github.com/hhhunnn>
2024-06-17 16:52:13 +0900

    reserva

---
+++ back/src/main/java/com/rentcar/back/dto/request/reservation/PatchReservationCancelRequestDto.java
@@ -0,0 +1,14 @@
+package com.rentcar.back.dto.request.reservation;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatchReservationCancelRequestDto {
+    @NotBlank
+    private String reservationState;
+}
