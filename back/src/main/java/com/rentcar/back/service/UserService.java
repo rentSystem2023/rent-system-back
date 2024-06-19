b67e49392f33260e41bd0c19de079cd86bab633a
devKMC <allcasa@naver.com>
2024-06-19 12:17:49 +0900

    2024-06-19 12:20

--- back/src/main/java/com/rentcar/back/service/UserService.java
+++ back/src/main/java/com/rentcar/back/service/UserService.java
@@ -16,18 +16,25 @@ import com.rentcar.back.dto.response.user.GetUserListResponseDto;
 @Service
 public interface UserService {
 
+    // 사용자 정보 가져오기
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser (String userId); 
 
+    // 사용자의 세부 정보 가져오기
     ResponseEntity<? super GetMyInfoResponseDto> getMyInfo (String userId);
 
+    // 비밀번호 수정하기
     ResponseEntity<ResponseDto> putPasswordModify (PutPwModifyRequestDto dto, String userId);
 
+    // 이메일 인증하기
     ResponseEntity<ResponseDto> emailAuth (EmailAuthRequestDto dto);
 
+    // 이메일 수정하기
     ResponseEntity<ResponseDto> putEmailModify (PutEmailModifyRequestDto dto, String userId);
 
+    // 회원 탈퇴하기
     ResponseEntity<ResponseDto> deleteMyInfo(String userId);
 
+    
     ResponseEntity<? super GetUserListResponseDto> getUserList (String userId);
 
     ResponseEntity<ResponseDto> deleteUserList(String userId);
